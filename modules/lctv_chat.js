b2b1eb4aea48f59dfe9a4ad0db7ede672d6d9199
Wolvan <wolvan1@gmail.com>
2015-11-09 23:46:04 +0100

    Multiple minor changes to chat_connector
    An argument "include_self" has been added that allows
    to have the bot emit events if it is responsible for them itself.
    
    Furthermore, a few more events are being directly forwarded
    now, in case they ever need to be hooked.
    
    A bit of debug code has been removed.
    
    The userlist object and the user arrays have been moved so
    that they are now accessible from the outside

--- modules/lctv_chat.js
+++ modules/lctv_chat.js
@@ -3,18 +3,21 @@ var xmpp = require("node-xmpp-client");
 var ltx = require("node-xmpp-core").ltx;
 
 //GENERATE DOCUMENTATION LATER!!!
-function chat_connector (config_or_username, password, channel, command_prefix) {
+function chat_connector (config_or_username, password, channel, command_prefix, include_self) {
 	
 	var self = this;
 	
-	var userlist = {}; var mods = []; users = [];
+	this.userlist = {}; this.mods = []; this.users = [];
 	
 	var username = config_or_username;
+	command_prefix = command_prefix || "!";
+	include_self = include_self || false;
 	if (typeof config_or_username === "object") {
 		username = config_or_username.username;
 		password = config_or_username.password;
 		channel = config_or_username.channel;
 		command_prefix = config_or_username.command_prefix || "!";
+		include_self = config_or_username.include_self || false;
 	}
 	if (channel.indexOf("@") === -1) {
 		channel += "@chat.livecoding.tv";
@@ -26,24 +29,21 @@ function chat_connector (config_or_username, password, channel, command_prefix) 
 	});
 
 	this.client.on("online", function() {
-		console.log("XMPP Client connected!");
 		var join_data = new ltx.Element("presence", {
 			to: channel + "/" + username
 		}).c("x", { xmlns: 'http://jabber.org/protocol/muc' });
 		self.client.send(join_data);
+		self.emit("online");
 	});
 	
 	this.client.on("stanza", function(stanza) {
-		// Manage different Stanzas here
+		self.emit("stanza", stanza);
 		var stanza_type = stanza.type;
 		var stanza_event
 		switch (stanza_type) {
 			case "error":
 				stanza_event = "error";
 				self.emit("error", stanza);
-				break;
-			case "result":
-				
 				break;
 			default:
 				stanza_event = stanza.name;
@@ -64,12 +64,18 @@ function chat_connector (config_or_username, password, channel, command_prefix) 
 						if (stanza_type !== "chat") {
 							from = from.substring(from.indexOf("/") + 1);
 						}
+						if (!include_self && (from.toLowerCase() === username.toLowerCase())) {
+							return;
+						}
 						self.emit("command", command, from, msg_split);
 						self.emit("command#" + command, from, msg_split);
 					} else {
 						var from = stanza.attrs.from;
 						if (stanza_type !== "chat") {
 							from = from.substring(from.indexOf("/") + 1);
+						}
+						if (!include_self && (from.toLowerCase() === username.toLowerCase())) {
+							return;
 						}
 						self.emit("message", from, msg);
 					}
@@ -78,40 +84,39 @@ function chat_connector (config_or_username, password, channel, command_prefix) 
 			case "presence":
 				var from = stanza.attrs.from;
 				from = from.substring(from.indexOf("/") + 1);
+				if (!include_self && (from.toLowerCase() === username.toLowerCase())) {
+					return;
+				}
 				var isAdmin = stanza.getChild("x").getChild("item").attrs.affiliation === "admin";
 				if (typeof stanza.attrs.type !== "undefined" && stanza.attrs.type === "unavailable") {
 					self.emit("part", from, isAdmin);
-					delete userlist[from];
+					delete self.userlist[from];
 					if (isAdmin) {
-						var index = mods.indexOf(from);
-						if (index > -1) { mods.splice(index, 1); }
+						var index = self.mods.indexOf(from);
+						if (index > -1) { self.mods.splice(index, 1); }
 						self.emit("admin_part", from);
 					} else {
-						var index = users.indexOf(from);
-						if (index > -1) { users.splice(index, 1); }
+						var index = self.users.indexOf(from);
+						if (index > -1) { self.users.splice(index, 1); }
 						self.emit("user_part", from);
 					}
 				} else {
 					self.emit("join", from, isAdmin);
-					userlist[from] = isAdmin;
+					self.userlist[from] = isAdmin;
 					if (isAdmin) {
-						mods.push(from);
+						self.mods.push(from);
 						self.emit("admin_join", from);
 					} else {
-						users.push(from);
+						self.users.push(from);
 						self.emit("user_join", from);
 					}
 				}
-				break;
-			default:
-				console.log(stanza);
 				break;
 		}
 	});
 	
 	this.client.on("end", function() {
-		console.log("XMPP Client closed, closing down this Bot Connection");
-		// Do closing logic, end program here?
+		self.emit("end");
 	});
 	
 	this.say = function (message) {
