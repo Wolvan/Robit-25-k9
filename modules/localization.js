b07f4ca18e4d93988c35ddf22d8a394fe7bd67ad
Wolvan <wolvan1@gmail.com>
2015-11-09 23:40:58 +0100

    Add special variables to replace
    There is certain information that should be replacable in
    localized strings. They will properly be replaced with
    information coming from config now and can be used
    with locales from the language file or just regular strings.
    
    Also, "localization:" can be added to any string passed to
    the function to make it more obvious that it comes from
    the localization file. This is optional though, and does not
    serve a purpose apart from making it clearer

--- modules/localization.js
+++ modules/localization.js
@@ -18,10 +18,23 @@ function localize(language, config, language_file) {
 	
 	this.translate = function() {
 		var locale = arguments[0] || "";
+		if (locale.toLowerCase().indexOf("localization:") === 0) {
+			locale = locale.substring(13);
+		}
 		var sentence = language_data[locale] || locale;
 		for (var i = 1; i < arguments.length; i++) {
 			sentence = sentence.replace(new RegExp("%" + i, "g"), arguments[i]);
 		}
+		sentence = sentence.replace(new RegExp("%favorite_language", "g"), config.get("favorites.language"));
+		sentence = sentence.replace(new RegExp("%favorite_framework", "g"), config.get("favorites.framework"));
+		sentence = sentence.replace(new RegExp("%favorite_ide", "g"), config.get("favorites.ide"));
+		sentence = sentence.replace(new RegExp("%favorite_viewer", "g"), config.get("favorites.viewer"));
+		sentence = sentence.replace(new RegExp("%favorite_music", "g"), config.get("favorites.music"));
+		sentence = sentence.replace(new RegExp("%channel", "g"), config.get("LCTV.channel"));
+		sentence = sentence.replace(new RegExp("%lctv_user", "g"), config.get("LCTV.username"));
+		sentence = sentence.replace(new RegExp("%task", "g"), config.get("current_task"));
+		sentence = sentence.replace(new RegExp("%tools", "g"), config.get("tools"));
+		sentence = sentence.replace(new RegExp("%song", "g"), config.get("current_music"));
 		return sentence;
 	}
 }
